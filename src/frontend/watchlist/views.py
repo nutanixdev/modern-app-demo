24e182d9d90b8eea9399976715dbdb890bd50673
Jose Gomez <pipoe2h@gmail.com>
2023-01-07 19:41:37 +0000

    Update: Check OpenShift headers

--- src/frontend/watchlist/views.py
+++ src/frontend/watchlist/views.py
@@ -19,6 +19,9 @@ SSL_VERIFY = settings.SSL_VERIFY
 def index(request):
     response = requests.get(URL, verify=SSL_VERIFY, timeout=5)
 
+    print(request.META)
+    print(request.headers)
+
     data = response.json()
 
     for video in data:
